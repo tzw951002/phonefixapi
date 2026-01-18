62a27d5b1c27f2c287bfcd2cda9c9bb7667f9eef
touteii <tzw951002@gmail.com>
2026-01-18 18:16:01 +0900

    new

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = 'http://13.237.170.178:8080';
 
-// export const API_BASE_URL = 'http://54.249.197.206:8000';
+export const API_BASE_URL = "https://mobileone-repair.jp:8000";
 
 
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+// export const API_BASE_URL = 'http://127.0.0.1:8000';
